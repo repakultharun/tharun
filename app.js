5fa47bcddfaaf4e6a7c87cbec8ed5b50f6146f85
repakultharun <repakultharun@meta.com>
2026-06-10 12:50:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Middleware to parse JSON bodies
 app.use(express.json());
- 
+  
 // Set port and verify_token
 const port = process.env.PORT || 3000;
 const verifyToken = process.env.vibecode;
